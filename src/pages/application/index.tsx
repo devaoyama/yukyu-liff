672d8f0b36684382f97ee732d6ac7140bda51e6f
devkeita <keita.mywork@gmail.com>
2020-12-25 13:15:41 +0900

    make application complete page (wip)

--- src/pages/application/index.tsx
+++ src/pages/application/index.tsx
@@ -8,6 +8,7 @@ import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
 import Button from "@material-ui/core/Button";
 import {Controller, useForm} from "react-hook-form";
+import {useRouter} from "next/router";
 
 const useStyles = makeStyles((theme) => ({
     paper: {
@@ -33,12 +34,14 @@ type TInput = {
 const Index = () => {
     const { register, handleSubmit, formState, control } = useForm<TInput>();
 
+    const router = useRouter();
+
     const classes = useStyles();
 
     const handleClick = handleSubmit(async (input) => {
         console.log(input)
         if (window.confirm('有給申請しますか？')) {
-            console.log('ok');
+            await router.push('/application/complete');
         }
     });
 
